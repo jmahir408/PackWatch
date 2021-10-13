1ca82fffcc0bc77a36c26028bdaa4fa4152613e4
jmahir408 <jmahir408@gmail.com>
2021-10-13 11:49:18 -0700

    bot updated

--- bot.js
+++ bot.js
@@ -1,16 +1,27 @@
-const Discord = require("discord.js")
+const { Client, Intents, Channel } = require('discord.js')
 require('dotenv').config()
-const client = new Discord.Client()
-TOKEN = process.env.BOT_TOKEN
+const token = process.env.BOT_TOKEN
 
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const prefix = '!';
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+    console.log('Ready!');
+});
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
+    console.log(`Logged in as ${client.user.tag}!`)
+  })
+  
 
-client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("pong");
-  }
-})
-
-client.login(process.env.TOKEN)
+  client.on('message', msg => { // Message function
+    if (msg.author.bot) return; // Ignore all bots
+    if (msg.content.startsWith(prefix)) return; // It always has to starts with the prefix which is '!'
+ 
+    if (msg.content.startsWith(prefix + "ping")) { // When a player does '!ping'
+      msg.channel.send("Pong!") // The bot will say @Author, Pong!
+    }
+ });
+// Login to Discord with your client's token
+client.login(token);
