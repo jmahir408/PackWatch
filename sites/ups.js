1739eb757669bf8c99035621bfd3c2da4ecb4e40
gurubac <gbrbachchu@gmail.com>
2021-10-14 15:44:15 -0700

    cache

--- sites/ups.js
+++ sites/ups.js
@@ -1,9 +1,47 @@
 const puppeteer = require("puppeteer");
-
+const minimal_args = [
+  '--autoplay-policy=user-gesture-required',
+  '--disable-background-networking',
+  '--disable-background-timer-throttling',
+  '--disable-backgrounding-occluded-windows',
+  '--disable-breakpad',
+  '--disable-client-side-phishing-detection',
+  '--disable-component-update',
+  '--disable-default-apps',
+  '--disable-dev-shm-usage',
+  '--disable-domain-reliability',
+  '--disable-extensions',
+  '--disable-features=AudioServiceOutOfProcess',
+  '--disable-hang-monitor',
+  '--disable-ipc-flooding-protection',
+  '--disable-notifications',
+  '--disable-offer-store-unmasked-wallet-cards',
+  '--disable-popup-blocking',
+  '--disable-print-preview',
+  '--disable-prompt-on-repost',
+  '--disable-renderer-backgrounding',
+  '--disable-setuid-sandbox',
+  '--disable-speech-api',
+  '--disable-sync',
+  '--hide-scrollbars',
+  '--ignore-gpu-blacklist',
+  '--metrics-recording-only',
+  '--mute-audio',
+  '--no-default-browser-check',
+  '--no-first-run',
+  '--no-pings',
+  '--no-sandbox',
+  '--no-zygote',
+  '--password-store=basic',
+  '--use-gl=swiftshader',
+  '--use-mock-keychain',
+];
 const scrape = async (trackingNumber) => {
   const browser = await puppeteer.launch({
     headless: true,
     defaultViewport: null,
+    args: minimal_args,
+    userDataDir: '../cache/'
   });
 
   let url = "https://www.ups.com/WebTracking?loc=en_US&Requester=DAN&tracknum=";
