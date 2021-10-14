8ee8451e647b24c935ad08c8335b455966c61f1d
gurubac <gbrbachchu@gmail.com>
2021-10-14 15:37:26 -0700

    add start script" "

--- sites/ups.js
+++ sites/ups.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer");
 
 const scrape = async (trackingNumber) => {
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     defaultViewport: null,
   });
 
